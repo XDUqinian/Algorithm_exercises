7bf40d7a59a48ec2c9dbb5b8d82fae02b26ea173
xduqinian <1841128521@qq.com>
2023-03-22 17:49:48 +0800

    2023/03/22

--- 面向对象/未命名5.cpp
+++ 面向对象/未命名5.cpp
@@ -2,6 +2,7 @@
 using namespace std;
 class People
 {
+	private: 
 	static int num;
 	protected:
 		int age,height,weight;
@@ -25,5 +26,13 @@ int main()
 	People a(1,2,3);
 	a.Show();
 	a.ShowNum();
+	People b(2,2,2);
+	b.Show();
+	a.ShowNum();
 	return 0;
 }
+/*
+静态成员要类内声明，类外定义。
+静态成员是同类对象所共享的成员。 
+我感觉设成private比较合适，但是没要求的话都行，public方便一点。 
+*/
